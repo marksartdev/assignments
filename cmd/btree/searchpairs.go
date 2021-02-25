91de3c8dd2ca3e03ce2e850d5cac6ebc75e0852e
Mark Sart <marksart.pro@gmail.com>
2021-02-25 13:13:08 +0300

    Create search

--- cmd/btree/searchpairs.go
+++ cmd/btree/searchpairs.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/marksartdev/assignments/pkg/btree"
 )
 
 // nolint:gomnd // It's temporary test.
 func main() {
+	k := 7
+
 	list1 := &btree.Node{Data: 3}
 	list2 := &btree.Node{Data: 13}
 	list3 := &btree.Node{Data: 7}
@@ -25,16 +27,68 @@ func main() {
 	list15 := &btree.Node{Data: 11, Left: list12, Right: list13}
 	root := &btree.Node{Data: 7, Left: list14, Right: list15}
 
-	res := bfSearch(root, 20)
-	log.Println(root.Data, res.Data)
+	pairs := make(map[[2]int]struct{})
+
+	pairCh := make(chan [2]int, 10)
+	exitCh := make(chan struct{}, 10)
+	goroutineCount := 0
+
+	var iterator btree.Iterator = root.GetIterator()
+	for iterator.HasNext() {
+		next := iterator.Next()
+		startGoroutine(pairCh, exitCh, root, next, k)
+		goroutineCount++
+	}
+
+	for goroutineCount > 0 {
+		select {
+		case pair := <-pairCh:
+			pairs[pair] = struct{}{}
+			goroutineCount--
+		case <-exitCh:
+			goroutineCount--
+		}
+	}
+
+	for pair := range pairs {
+		fmt.Println(pair) // nolint:forbidigo // It's for printing results.
+	}
 }
 
-func bfSearch(root *btree.Node, k int) *btree.Node {
+func startGoroutine(pairCh chan [2]int, exitCh chan struct{}, root, base *btree.Node, k int) {
+	go func() {
+		if res := bfSearch(root, getCheckFunc(base, k)); res != nil {
+			sendPair(pairCh, base.Data, res.Data)
+		} else {
+			exitCh <- struct{}{}
+		}
+	}()
+}
+
+func getCheckFunc(base *btree.Node, k int) func(node *btree.Node) bool {
+	return func(node *btree.Node) bool {
+		if node == base {
+			return false
+		}
+
+		return base.Data+node.Data == k
+	}
+}
+
+func sendPair(pairCh chan [2]int, a, b int) {
+	if a < b {
+		pairCh <- [2]int{a, b}
+	} else {
+		pairCh <- [2]int{b, a}
+	}
+}
+
+func bfSearch(root *btree.Node, check func(node *btree.Node) bool) *btree.Node {
 	iterator := root.GetIterator()
 	for iterator.HasNext() {
 		node := iterator.Next()
 
-		if root.Data+node.Data == k {
+		if check(node) {
 			return node
 		}
 	}
